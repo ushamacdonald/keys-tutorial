09f7b0b0ed30e3b7c61face14206496849643951
ushamacdonald <usha@springload.co.nz>
2018-07-13 09:44:06 +1200

    Add new main screen

--- App.js
+++ App.js
@@ -1,26 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Provider} from 'react-redux';
-import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator} from 'react-navigation'
 
 import store from './store';
+import MainScreen from './screens/MainScreen'
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
+    const MainNavigator = createStackNavigator({
+      Main: {screen: MainScreen}
+    });
+
     return (
       <Provider store={store}>
-        <View style={styles.container}>
-          <Text>Test</Text>
-        </View>
+        <MainNavigator />
       </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
