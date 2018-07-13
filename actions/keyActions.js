0f01024dc208fc343946931cf5ba148fdebbcecd
ushamacdonald <usha@springload.co.nz>
2018-07-13 16:12:25 +1200

    Formatting

--- actions/keyActions.js
+++ actions/keyActions.js
@@ -1,7 +1,4 @@
-import {
-  SELECT_KEY_INDEX,
-  SELECT_CAPO
-} from './types'
+import { SELECT_KEY_INDEX, SELECT_CAPO } from './types';
 
-export const selectKeyIndex = payload => ({type: SELECT_KEY_INDEX, payload});
-export const selectCapo = payload => ({type: SELECT_CAPO, payload});
+export const selectKeyIndex = payload => ({ type: SELECT_KEY_INDEX, payload });
+export const selectCapo = payload => ({ type: SELECT_CAPO, payload });
