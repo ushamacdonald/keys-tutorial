9b2e136227348d1868d58dac90bfe28fbca8dda4
ushamacdonald <usha@springload.co.nz>
2018-07-16 10:49:58 +1200

    Add new selection reducer logic for capo key

--- reducers/selectionsReducer.js
+++ reducers/selectionsReducer.js
@@ -2,21 +2,30 @@ import { SELECT_KEY_INDEX, SELECT_CAPO } from '../actions/types';
 
 const INITIAL_STATE = {
   selectedKeyIndex: 0,
-  selectedCapo: 4
+  selectedCapo: 4,
+  capoKeyIndex: 4
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case SELECT_KEY_INDEX: {
+      const capoKeyIndex = state.selectedCapo + action.payload;
+      if (capoKeyIndex >= 12) capoKeyIndex -= 12;
+
       return {
         ...state,
-        selectedKeyIndex: action.payload
+        selectedKeyIndex: action.payload,
+        capoKeyIndex
       };
     }
     case SELECT_CAPO: {
+      const capoKeyIndex = state.selectedKeyIndex + action.payload;
+      if (capoKeyIndex >= 12) capoKeyIndex -= 12;
+
       return {
         ...state,
-        selectedCapo: action.payload
+        selectedCapo: action.payload,
+        capoKeyIndex
       };
     }
     default:
