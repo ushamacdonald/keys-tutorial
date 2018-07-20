247b588b1e80e1a505e0193a9a702fa5ef11b96f
ushamacdonald <usha.macdonald@gmail.com>
2018-07-20 14:10:03 +1200

    Remove console log

--- actions/modalActions.js
+++ actions/modalActions.js
@@ -1,12 +1,9 @@
 import { OPEN_CHORDS_MODAL, CLOSE_CHORDS_MODAL } from './types';
 
-export const openChordsModal = () => {
-  console.log("Open modal");
-  return {
-    type: OPEN_CHORDS_MODAL,
-    payload: true
-  }
-};
+export const openChordsModal = () => ({
+  type: OPEN_CHORDS_MODAL,
+  payload: true
+});
 export const closeChordsModal = () => ({
   type: CLOSE_CHORDS_MODAL,
   payload: false
